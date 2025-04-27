3334b1bd2abe959be7c9a784d6576ab114fb4791
Tataaah <tataspocket@gmail.com>
2025-04-26 23:36:07 -0300

    Delete src directory

--- src/utils/formatDate.ts
+++
@@ -1,9 +0,0 @@
-export function getCurrentDate(): string {
-  const now = new Date();
-  
-  const day = String(now.getDate()).padStart(2, '0');
-  const month = String(now.getMonth() + 1).padStart(2, '0');
-  const year = now.getFullYear();
-  
-  return `${day}/${month}/${year}`;
-}
